d83dfebba38ad15cf0840226d385db06492944ff
sangzhenya <sangzhenya@gmail.com>
2020-01-10 22:42:03 +0800

    Add more info of provider and consumer

--- iboot/producer/src/main/java/com/xinyue/producer/controller/IndexController.java
+++ iboot/producer/src/main/java/com/xinyue/producer/controller/IndexController.java
@@ -2,16 +2,26 @@ package com.xinyue.producer.controller;
 
 import com.xinyue.api.UserService;
 import com.xinyue.model.User;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class IndexController {
+    private Log log = LogFactory.getLog(IndexController.class);
+    private final DiscoveryClient client;
+
     private final UserService userService;
 
-    public IndexController(UserService userService) {
+    public IndexController(UserService userService, DiscoveryClient client) {
         this.userService = userService;
+        this.client = client;
     }
 
     @GetMapping("/user")
@@ -32,5 +42,16 @@ public class IndexController {
         user.setFirstName("Lin");
         user.setLastName("Xinyue");
         return userService.createUser(user).toString();
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = client.getServices();
+        log.info(services);
+        List<ServiceInstance> instances = client.getInstances("IBOOT-PROVIDER");
+        for (ServiceInstance instance : instances) {
+            log.info(instance);
+        }
+        return this.client;
     }
 }
