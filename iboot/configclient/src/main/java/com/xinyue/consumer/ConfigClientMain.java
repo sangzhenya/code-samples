763719388f82ef8bc21319f5b5126ab130edd602
sangzhenya <sangzhenya@gmail.com>
2020-01-11 23:15:36 +0800

    add config client

---
+++ iboot/configclient/src/main/java/com/xinyue/consumer/ConfigClientMain.java
@@ -0,0 +1,11 @@
+package com.xinyue.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain.class, args);
+    }
+}
---
+++ iboot/configclient/src/main/java/com/xinyue/consumer/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.xinyue.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Value("${server.port}")
+    private String port;
+
+    @ResponseBody
+    @GetMapping("/")
+    public String index() {
+        return port;
+    }
+
+}
